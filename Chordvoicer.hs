b16fac47b7313c7db238223496ddb6f84defea9e
Stefan Matting <stefan@wire.com>
2023-06-21 13:00:49 +0200

    wip

--- Chordvoicer.hs
+++ Chordvoicer.hs
@@ -3,9 +3,10 @@ module Chordvoicer  where
 import Control.Applicative ((<|>), many)
 import Data.Functor (($>), void)
 import Data.List (takeWhile, findIndex, drop, break, elem)
-import Data.Maybe (listToMaybe, catMaybes, fromJust)
-import Debug.Trace (traceM)
+import Data.Maybe (listToMaybe, catMaybes, fromJust, mapMaybe)
+import Debug.Trace (traceM, trace)
 import Data.String
+import Data.Foldable (foldl')
 import Sound.Tidal.Core
 import Sound.Tidal.ParseBP as Tidal (parseNote)
 import Sound.Tidal.Pattern (Pattern)
@@ -17,6 +18,10 @@ import Text.ParserCombinators.ReadPrec hiding (look, get)
 import Text.Read hiding (look, get)
 import qualified Sound.Tidal.Pattern as Tidal
 import qualified Text.Parsec as Parsec
+
+--
+-- chord voicer core
+--
 
 -- A note within an implied scale idx=1 first note of the scale, idx=2 second note
 -- deltas are semitones to go off-scale
@@ -53,22 +58,24 @@ add i k n =
 -- Smallest k > 0
 -- so that add i k n == t
 solveAddPos :: Int -> Int -> Int -> Int
-solveAddPos i t n =
+solveAddPos n i t =
   let d = t - i in
     if d <= 0 then d + n else d
 
 -- Biggest k < 0
 -- so that add(i, k, n) == t
 solveAddNeg :: Int -> Int -> Int -> Int
-solveAddNeg i t n =
+solveAddNeg n i t =
   let d = t - i in
     if d >= 0 then d - n else d
 
-noteAdd :: Note -> Int -> Int -> Int -> Note
-noteAdd (Note i _ o) k n d =
+noteAdd :: Int -> Note -> Int -> Int -> Note
+noteAdd n (Note i _ o) k d =
   let ii = i - 1 + k
-      dj = if ii < 0 then -1 else (if ii >= n then 1 else 0)
-  in Note ((ii `mod` n) + 1) d (o + dj)
+      dj | ii < 0    = -1
+         | ii >= n   = 1
+         | otherwise = 0
+  in Note (add i k n) d (o + dj)
 
 splitlohi :: [VoiceNote] -> ([VoiceNote], [VoiceNote])
 splitlohi vnotes =
@@ -81,38 +88,41 @@ voice :: Note -> [VoiceNote] -> Int -> [Note]
 voice rootNote vnotes n =
   let (vnlo, vnhi) = splitlohi vnotes
       (_, _, notesLo) =
-            foldr (\v (i, nt, nl) ->
+            foldl' (\(i, nt, nl) v ->
                    if isDot v
                      then (i, nt, nl)
                    else
                      if isJump v
-                       then (i, noteAdd nt (-n) n (vdelta v + delta rootNote), nl)
+                       then (i, noteAdd n nt (-n) (vdelta v + delta rootNote), nl)
                      else
                        let k = solveAddNeg i (vidx v) n
-                           i = vidx v
-                           nt = noteAdd nt k n (vidx v + delta rootNote)
-                       in (i, nt, nt:nl)
+                           i' = vidx v
+                           nt' = noteAdd n nt k (vdelta v + delta rootNote)
+                       in (i', nt', nt':nl)
                    )
             (1, rootNote, [])
             (reverse vnlo)
 
       (_, _, notesHi) =
-            foldr (\v (i, nt, nl) ->
+            foldl' (\(i, nt, nl) v ->
                    if isDot v
                      then (i, nt, nl)
                    else
                      if isJump v
-                       then (i, noteAdd nt n n (vdelta v + delta rootNote), nl)
+                       then (i, noteAdd n nt n (vdelta v + delta rootNote), nl)
                      else
-                       let k = solveAddPos i (vidx v) n
-                           i = vidx v
-                           nt = noteAdd nt k n (vidx v + delta rootNote)
-                       in (i, nt, nt:nl)
+                       let k = solveAddPos n i (vidx v)
+                           i' = vidx v
+                           nt' = noteAdd n nt k (vdelta v + delta rootNote)
+                       in (i', nt', nt':nl)
                    )
-            (n, noteAdd rootNote (-1) n (delta rootNote), [])
+            (n, noteAdd n rootNote (-1) (delta rootNote), [])
             vnhi
   in reverse notesLo <> reverse notesHi
 
+---
+--- Parsers
+---
 
 parsePositive :: ReadP Int
 parsePositive = do
@@ -183,7 +193,6 @@ parseScale = do
     readJust msg Nothing = fail msg
     readJust _msg (Just x) = pure x
 
-
 pnote :: String -> Maybe Tidal.Note
 pnote s = either (const Nothing) Just  $ Parsec.runParser Tidal.parseNote 0 "" s
 
@@ -209,8 +218,17 @@ runParser parser str = fst <$> listToMaybe (readPrec_to_S (readP_to_Prec (const 
 parse :: String -> Maybe (Note, [VoiceNote])
 parse = runParser parseChord
 
-gscale :: String -> Maybe [Int]
-gscale s = maybe Nothing (Just . map round) (lookup s scaleTable)
+---
+--- chordvoicer tidal integration
+---
+
+mkscale :: Pattern String -> Pattern [Tidal.Note]
+mkscale = fmap (\s ->
+                  case runParser parseScale s of
+                    Nothing -> []
+                    Just (Just baseNote, notes) -> fmap (+ baseNote) notes
+                    Just (Nothing, notes) -> notes
+               )
 
 applyScale :: Int -> [Tidal.Note] -> Note -> Maybe Tidal.Note
 applyScale n scale (Note idx d o) =
@@ -221,16 +239,18 @@ applyScale n scale (Note idx d o) =
       Just $ Tidal.Note $ Tidal.unNote (scale !! i) + fromIntegral d + fromIntegral o * 12
     else Nothing
 
-vchord :: Pattern [Tidal.Note] -> Pattern String -> Pattern Tidal.Note
-vchord scaleP voicesP = Tidal.uncollect $ do
-   mvoices <- parse <$> voicesP
-   scale <- scaleP
-   let n = length scale
-   case mvoices of
-     Nothing -> pure []
-     Just (rootNote, vs) ->
-       let notes = voice rootNote vs n
-       in pure $ catMaybes (map (applyScale n scale) notes)
+inscale :: Pattern [Tidal.Note] -> Pattern String -> Pattern [Tidal.Note]
+inscale scaleP voicesP = do
+  -- traceM ("inscale: ")
+  mvoices <- parse <$> voicesP
+  scale <- scaleP
+  -- traceM ("inscale: scale: " <> show scale)
+  let n = length scale
+  case mvoices of
+    Nothing -> pure []
+    Just (rootNote, vs) ->
+      let notes = voice rootNote vs n
+      in pure $ mapMaybe (applyScale n scale) notes
 
 -- Tests
 
@@ -259,6 +279,11 @@ testParse parser input expected =
     Just x -> let msg = ("Failed to parse correctly: " <> input <> "\nExpected: " <> show expected <> "\nBut got : " <> show x <> "\n")
               in assertBool msg (x == expected)
 
+testChordVoicer :: Int -> Note -> [VoiceNote] ->  [Note] -> IO ()
+testChordVoicer n baseNote vs expected = do
+  let actual = voice baseNote vs n
+      msg = "Expected: " <> show expected <> "\nBug got : " <> show actual <> "\n"
+  assertBool msg (expected == actual)
 
 tests :: IO ()
 tests = do
@@ -278,3 +303,15 @@ tests = do
   testParse parseChord "1:1-3-5" (Note 1 0 0, [VoiceNote 1 0, VoiceNote 3 0,  VoiceNote 5 0])
   testParse parseScale "c4:ionian" (Just (fromJust (pnote "c4") :: Tidal.Note),  [0, 2, 4, 5, 7, 9, 11] :: [Tidal.Note])
   testParse parseScale "ionian" (Nothing,  [0, 2, 4, 5, 7, 9, 11] :: [Tidal.Note])
+
+  testChordVoicer
+         7
+        (Note 1 0 0)
+        [VoiceNote 1 0, VoiceNote 3 0, VoiceNote 5 0]
+        [Note 1 0 0, Note 3 0 0, Note 5 0 0]
+
+  testChordVoicer
+         7
+        (Note 1 1 0)
+        [VoiceNote 1 (-1), VoiceNote 3 1, VoiceNote 5 0]
+        [Note 1 0 0, Note 3 2 0, Note 5 1 0]
